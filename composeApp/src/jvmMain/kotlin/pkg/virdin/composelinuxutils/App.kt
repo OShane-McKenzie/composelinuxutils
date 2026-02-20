fd12ec705ac94eafdd87b01dcdc342fb10c2979d
OShane-McKenzie <oshanemckenzie5@gmail.com>
2026-02-20 02:08:40 -0500

    UPDATE LICENSE

--- composeApp/src/jvmMain/kotlin/pkg/virdin/composelinuxutils/App.kt
+++ composeApp/src/jvmMain/kotlin/pkg/virdin/composelinuxutils/App.kt
@@ -1,48 +1,48 @@
 package pkg.virdin.composelinuxutils
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ModalDrawer
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Apps
-import androidx.compose.material3.Badge
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
-
-import composelinuxutils.composeapp.generated.resources.Res
-import composelinuxutils.composeapp.generated.resources.compose_multiplatform
-import kotlinx.coroutines.launch
-
+//import androidx.compose.animation.AnimatedVisibility
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRow
+//import androidx.compose.foundation.layout.PaddingValues
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.safeContentPadding
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.layout.width
+//import androidx.compose.foundation.lazy.grid.GridCells
+//import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+//import androidx.compose.foundation.lazy.grid.items
+//import androidx.compose.foundation.shape.CircleShape
+//import androidx.compose.material.ModalDrawer
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Apps
+//import androidx.compose.material3.Badge
+//import androidx.compose.material3.Button
+//import androidx.compose.material3.Icon
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.clip
+//import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.text.style.TextOverflow
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import org.jetbrains.compose.resources.painterResource
+//
+//import composelinuxutils.composeapp.generated.resources.Res
+//import composelinuxutils.composeapp.generated.resources.compose_multiplatform
+//import kotlinx.coroutines.launch
+//
 //@Composable
 //@Preview
 //fun App() {
